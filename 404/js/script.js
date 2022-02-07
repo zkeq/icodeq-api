f0ff1b0a59c096a090d6a8b3f2c1c13de3c9d672
Zkeq <learnonly@learnonly.xyz>
2022-02-07 10:09:53 +0800

    API 首页 404

---
+++ 404/js/script.js
@@ -0,0 +1,17 @@
+var percentageElement = document.getElementById("percentage");
+var percentage = 0;
+
+function process() {
+  percentage += parseInt(Math.random() * 10);
+  if (percentage > 100) {
+    percentage = 100;
+    window.location.replace("/");
+  }
+  percentageElement.innerText = percentage;
+  processInterval();
+}
+
+function processInterval() {
+  setTimeout(process, Math.random() * (1000 - 500) + 500);
+}
+processInterval();
