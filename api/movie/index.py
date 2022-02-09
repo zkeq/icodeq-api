8577a4e7dc0f31f290d1cdc99cf7b8686ed69e14
Zkeq <learnonly@learnonly.xyz>
2022-02-09 14:58:45 +0800

    Update index.py

--- api/movie/index.py
+++ api/movie/index.py
@@ -4,6 +4,10 @@ import re
 from http.server import BaseHTTPRequestHandler
 import json
 
+html_template = """
+
+
+
 
 def list_split(items, n):
     return [items[i:i + n] for i in range(0, len(items), n)]
