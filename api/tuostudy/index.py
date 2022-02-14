f665a7265b641bf76714013ed4493f49d5518362
Zkeq <learnonly@learnonly.xyz>
2022-02-14 15:39:41 +0800

    使用josn替代txt

--- api/tuostudy/index.py
+++ api/tuostudy/index.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+import json
 import requests
 from http.server import BaseHTTPRequestHandler
 
@@ -11,28 +12,19 @@ def read_file(file_name):
 
 def url_2_html(url):
     html_file = read_file('./api/tuostudy/temp.html')
-    url = url.replace('\n', '')
     html_file = html_file.replace('{{url}}', url)
     return html_file
 
 
 def get_308(name):
-    url = 'http://tuo-site.oss-cn-beijing.aliyuncs.com/index.txt'
+    url = 'http://tuo-site.oss-cn-beijing.aliyuncs.com/data.json'
     r = requests.get(url)
-    r.encoding = 'utf-8'
-    _str = r.text
-    _list = _str.split('\n')
-    name_list = []
-    url_list = []
-    for i in _list:
-        line_list = i.split('|||')
-        name_list.append(line_list[0])
-        url_list.append(line_list[1])
-    final_url = 'https://tuostudy.vercel.app/'
-    for i in name_list:
-        if i == name:
-            final_url = url_list[name_list.index(i)]
-    data = url_2_html(final_url)
+    _data = json.loads(r.text)
+    try:
+        url = _data[name]
+    except KeyError:
+        url = 'https://tuostudy.vercel.app/'
+    data = url_2_html(url)
     return data
 
 
