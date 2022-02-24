2edd682e3dbdc8909037674a7c52068da5f4a2e4
Zkeq <learnonly@learnonly.xyz>
2022-02-24 10:43:27 +0800

    测试

--- api/get_163_mv/test.py
+++ api/get_163_mv/test.py
@@ -1,13 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import os
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome()
 driver.get("https://www.baidu.com")
 print(driver.title)
 driver.quit()
