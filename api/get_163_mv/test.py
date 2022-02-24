6cd65bebe96e9ba355206b31f9514155d374f1ec
Zkeq <learnonly@learnonly.xyz>
2022-02-24 10:41:49 +0800

    Update test.py

--- api/get_163_mv/test.py
+++ api/get_163_mv/test.py
@@ -7,9 +7,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chromedriver = "/usr/bin/chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.get("https://www.baidu.com")
 print(driver.title)
 driver.quit()
