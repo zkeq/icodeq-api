dffe5fb86b75bc40b0bf1a71f00eeeb6b51d08f6
Zkeq <learnonly@learnonly.xyz>
2022-02-24 10:36:31 +0800

    测试1 63

--- api/get_163_mv/local_main.py
+++ api/get_163_mv/local_main.py
@@ -12,7 +12,7 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-chromedriver = "/usr/bin/chromedriver"
+chromedriver = "/usr/bin/google-chrome"
 os.environ["webdriver.chrome.driver"] = chromedriver
 
 r = redis.Redis(
